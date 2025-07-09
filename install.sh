27ed7d75a495c9895c6bd4f2275ae42596f202ea
Zain Farhan <zainfarhan.work@gmail.com>
2025-07-09 20:26:29 +0500

    Nvidia support 2 + Waybar 1

--- install.sh
+++ install.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-# GPU Setup
-
-#!/bin/bash
-
-echo "Do you want to configure for NVIDIA GPU? (y/n)"
-read -r use_nvidia
-
-if [[ "$use_nvidia" == "y" || "$use_nvidia" == "Y" ]]; then
-    echo "Setting up for NVIDIA GPU..."
-
-    # Install NVIDIA packages
-    sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings --noconfirm
-
-    # Enable Modules for Nvidia
-    sudo sed -i 's/^MODULES=.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-    sudo mkinitcpio -P
-
-    echo "env = GBM_BACKEND,nvidia-drm" >> ./hypr/hyprland.conf
-
-    # NVIDIA-specific Hyprland config (optional)
-    echo "env = LIBVA_DRIVER_NAME,nvidia" >> ./hypr/hyprland.conf
-    echo "env = __GLX_VENDOR_LIBRARY_NAME,nvidia" >> ./hypr/hyprland.conf
-
-    echo "NVIDIA setup done. Reboot after install."
-else
-    echo "Skipping NVIDIA setup."
-fi
-
-
 # Basic setup
 echo "Updating System"
 sudo pacman -Syu
@@ -37,7 +8,27 @@ sudo pacman -S hyprland waybar kitty rofi --noconfirm
 
 # Symbolic Links
 echo "Creating Symbolic Links"
-ln -s ~/Arch-Hyprland/hypr ~/.config/hypr
+ln -s ~/Arch-Hyprland/config/hypr ~/.config/hypr
+
+# GPU Setup
+echo "Do you want to configure for NVIDIA GPU? (y/n)"
+read -r use_nvidia
+
+if [[ "$use_nvidia" == "y" || "$use_nvidia" == "Y" ]]; then
+    echo "Installing NVIDIA Drivers"
+    sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings --noconfirm
+
+    sudo sed -i 's/^MODULES=.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo mkinitcpio -P
+
+    echo "Linking NVIDIA environment config"
+    ln -sf ~/Arch-Hyprland/hypr/env/nvidia.conf ~/.config/hypr/env.conf
+else
+    echo "Removing NVIDIA environment config if it exists"
+    rm -f ~/.config/hypr/env.conf
+fi
+
+ln -s ~/Arch-Hyprland/config/waybar ~/.config/waybar
 
 # Set zsh as default
 echo "Changing Shell"
